1995b5c5c5d00ebe9c38b2a83eccf9a3d9c17ce6
Salokled <nikolasmartins@hotmail.com>
2024-06-21 18:10:23 -0300

    the last commit

--- exercícios 19_04_2024/4.py
+++ exercícios 19_04_2024/4.py
@@ -1,6 +1,6 @@
-preco1= float(input("Digite o primeiro preço R$:"))
-preco2= float(input("Digite o segundo preço R$:"))
-preco3= float(input("Digite o terceiro preço R$:"))
+preco1 = float(input("Digite o primeiro preço R$:"))
+preco2 = float(input("Digite o segundo preço R$:"))
+preco3 = float(input("Digite o terceiro preço R$:"))
 
 if preco1 < preco2 and preco1 < preco3:
     print (f"Você deve comprar o produto que custa R${preco1:.2f}")
