0adc1d99d13f1c4acd564817b90757998c766d51
Salokled <nikolasmartins@hotmail.com>
2024-06-21 17:16:27 -0300

    resolução

--- exercícios 19_04_2024/3.py
+++ exercícios 19_04_2024/3.py
@@ -1,1 +1,16 @@
-num2
+num1 = float(input("Insira a primeira nota do aluno:"))
+num2 = float (input("Insira a segunda nota do aluno:"))
+
+media = (num1 + num2) / 2
+
+if media >= 7 and media < 10:
+    print("Aprovado")
+    
+elif media < 7:
+    print ("Reprovado")
+
+else:
+    print ("Aprovado com distinção!")
+    
+
+
