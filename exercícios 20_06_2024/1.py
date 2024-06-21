ab17c42ccd0c006b305239ee8ab8b8c067c8d14f
Salokled <nikolasmartins@hotmail.com>
2024-06-21 19:24:04 -0300

    The last commit

---
+++ exercícios 20_06_2024/1.py
@@ -0,0 +1,18 @@
+salario = float(input("Informe seu salário R$: "))
+
+if salario <= 280:
+        percentual = 20
+elif salario <= 700:
+        percentual = 15
+elif salario <= 1500:
+        percentual = 10
+else:
+        percentual = 5
+
+salarioANTERIOR = salario * (percentual / 100)
+salarioNOVO = salario + salarioANTERIOR
+
+print(f"\nO percentual de aumento para seu salário é:{percentual}%")
+print(f"Salário antes do reajuste era R${salario:.2f}")
+print(f"Valor incrementado ao novo salário R${salarioANTERIOR:.2f}")
+print(f"Seu salário atual é de R${salarioNOVO:.2f}")
